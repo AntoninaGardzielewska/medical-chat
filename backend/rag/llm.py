98c19340b1dc58f19cc2f0940e1a9fcf9d4ed6bd
Antonina <tosiagardzielewska@gmail.com>
2026-06-04 21:25:57 +0200

    fix: update dockerfile

--- backend/rag/llm.py
+++ backend/rag/llm.py
@@ -1,7 +1,9 @@
-import httpx
 import os
 
+import httpx
+
 OLLAMA_BASE_URL = os.getenv("OLLAMA_BASE_URL", "http://localhost:11434")
+
 
 class OllamaChat:
     def __init__(self, model: str = "llama3.2:latest"):
@@ -12,22 +14,19 @@ class OllamaChat:
         try:
             response = self.client.post(
                 f"{OLLAMA_BASE_URL}/api/generate",
-                json={
-                    "model": self.model,
-                    "prompt": prompt,
-                    "stream": False
-
-                },
+                json={"model": self.model, "prompt": prompt, "stream": False},
             )
             response.raise_for_status()
         except (httpx.RequestError, httpx.HTTPStatusError, httpx.ReadTimeout) as e:
-            raise RuntimeError(f"Ollama is unreachable or returned an error: {e}")
-        
+            raise RuntimeError(
+                f"Ollama is unreachable or returned an error: {e}"
+            ) from e
+
         return response.json()["response"]
 
     def rewrite_user_query(self, user_question: str) -> str:
         prompt = f"""
-            You are a medical research asistent. 
+            You are a medical research asistent.
             Rewrite the following question into precise clinical language suitable for searching PubMed literature.
             Return only the rewritten query, nothing else.
             Question: {user_question}
